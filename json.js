6c5c0271f2cf55218a19dd7d2896fc9fd426b5da
ituberus <34318384+ituberus@users.noreply.github.com>
2024-07-06 18:51:05 +0100

    Add files via upload

---
+++ json.js
@@ -0,0 +1,6 @@
+const wifiData = [
+  {"name": "MT5G-3D9A18", "password": "xxxxxxxx"},
+  {"name": "Xfinity Home", "password": "galaxy5382"},
+  {"name": "TPLnk Fi", "password": "xxxxxxxx"},
+  {"name": "Stan Private", "password": "xxxxxxxx"}
+];
