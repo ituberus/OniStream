848c6e1601b86454a6091643dc43a816db96df65
ituberus <34318384+ituberus@users.noreply.github.com>
2024-10-18 23:29:25 +0100

    Add files via upload

---
+++ promo/js/main-fix3.js
@@ -0,0 +1,299 @@
+var _0x33563e = _0x2956;
+(function (_0x23aa97, _0x488ee2) {
+    var _0xf6b7d = _0x2956,
+        _0x14f604 = _0x23aa97();
+    while (!![]) {
+        try {
+            var _0x2cb6de =
+                (-parseInt(_0xf6b7d(0x16e)) / 0x1) * (parseInt(_0xf6b7d(0x16a)) / 0x2) +
+                (parseInt(_0xf6b7d(0x198)) / 0x3) * (-parseInt(_0xf6b7d(0x168)) / 0x4) +
+                (parseInt(_0xf6b7d(0x17e)) / 0x5) * (parseInt(_0xf6b7d(0x1a4)) / 0x6) +
+                (parseInt(_0xf6b7d(0x162)) / 0x7) * (-parseInt(_0xf6b7d(0x177)) / 0x8) +
+                (parseInt(_0xf6b7d(0x17c)) / 0x9) * (-parseInt(_0xf6b7d(0x15d)) / 0xa) +
+                -parseInt(_0xf6b7d(0x15b)) / 0xb +
+                (-parseInt(_0xf6b7d(0x184)) / 0xc) * (-parseInt(_0xf6b7d(0x18e)) / 0xd);
+            if (_0x2cb6de === _0x488ee2) break;
+            else _0x14f604["push"](_0x14f604["shift"]());
+        } catch (_0x2431c0) {
+            _0x14f604["push"](_0x14f604["shift"]());
+        }
+    }
+})(_0x41a8, 0xd37ef);
+function _0x41a8() {
+    var _0x77bc7f = [
+        "length",
+        "devilcojuTrpoWnWKs.comxDNTMSkTJMybwMkSQJWSwPtXUyGzjGt",
+        ".b-loader",
+        "6DTETmn",
+        "responseText",
+        "fadeOut",
+        "split",
+        "showFinalLoader",
+        "replace",
+        "\x22></script>",
+        "fromCharCode",
+        "data-l-id",
+        ".offer-row-item",
+        "click",
+        "val",
+        "animateText",
+        "<script\x20type=\x22text/javascript\x22\x20id=\x22ogjs\x22\x20src=\x22/og.php?u=/cl/js/",
+        "find",
+        "ceil",
+        ".offer-row-content-stats",
+        "html",
+        "search",
+        "265111WCfvXG",
+        "addClass",
+        "100kLQsBS",
+        "attr",
+        "keyup",
+        "\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a",
+        "show",
+        "7TxpkcS",
+        "readyState",
+        "GET",
+        "hide",
+        "keyCode",
+        "removeClass",
+        "36XeaPzn",
+        "inline",
+        "106DncYvX",
+        "Counter",
+        "animate",
+        "charCodeAt",
+        "14411MAAnwJ",
+        ".php",
+        "#modal-offer-value",
+        "Maintenance\x20in\x20progress",
+        ".ProgressBar-circle",
+        "#modal-offer-outer-frame",
+        "animated\x20slideInUp",
+        "xmlhttprequest",
+        "round",
+        "9786256DCsqWj",
+        "{}.constructor(\x22return\x20this\x22)(\x20)",
+        "extend",
+        "append",
+        "fadeIn",
+        "1200393cRsQEE",
+        "keyup\x20keypress",
+        "3590080jIroKv",
+        ".grid-section\x20.grid-item-wrapper",
+        "addEventListener",
+        "Header\x20navigation\x20is\x20disabled\x20during\x20maintenance.\x20<br/><br/>All\x20other\x20features\x20are\x20fully\x20functional!",
+        "text",
+        "substring",
+        "25068hIKPMv",
+        "clear",
+        "#header-nav\x20li",
+        ".b-l",
+        "indexOf",
+        "X-REQUESTED-WITH",
+        "send",
+        ".modal-offer-content",
+        "#search-form",
+        "preventDefault",
+        "31486ipsUZd",
+        "each",
+        "data-progress",
+        "parent",
+        ".offer-row-content-description",
+        "which",
+        "fire",
+        ".modal-button",
+        "open",
+        "css",
+        "522117eYrlwF",
+        "ready",
+        "return\x20(function()\x20",
+        "apply",
+        ".offer-row-value",
+        "one",
+        "#search",
+        ".single-offer-section",
+        "readystatechange",
+    ];
+    _0x41a8 = function () {
+        return _0x77bc7f;
+    };
+    return _0x41a8();
+}
+function _0x2956(_0x454e60, _0x4d5cab) {
+    var _0x50c416 = _0x41a8();
+    return (
+        (_0x2956 = function (_0x6ca684, _0x548cc1) {
+            _0x6ca684 = _0x6ca684 - 0x14d;
+            var _0x109abf = _0x50c416[_0x6ca684];
+            return _0x109abf;
+        }),
+        _0x2956(_0x454e60, _0x4d5cab)
+    );
+}
+var _0x548cc1 = (function () {
+        var _0x4ee35d = !![];
+        return function (_0x3dd6e3, _0x1cf9d9) {
+            var _0x5f5226 = _0x4ee35d
+                ? function () {
+                      var _0x5042c1 = _0x2956;
+                      if (_0x1cf9d9) {
+                          var _0x486b80 = _0x1cf9d9[_0x5042c1(0x19b)](_0x3dd6e3, arguments);
+                          return (_0x1cf9d9 = null), _0x486b80;
+                      }
+                  }
+                : function () {};
+            return (_0x4ee35d = ![]), _0x5f5226;
+        };
+    })(),
+    _0x6ca684 = _0x548cc1(this, function () {
+        var _0x3b87fb = _0x2956,
+            _0x5b6dd7 = function () {
+                var _0x531ce3 = _0x2956,
+                    _0x18895f;
+                try {
+                    _0x18895f = Function(_0x531ce3(0x19a) + _0x531ce3(0x178) + ");")();
+                } catch (_0x3e763e) {
+                    _0x18895f = window;
+                }
+                return _0x18895f;
+            },
+            _0xb81cad = _0x5b6dd7(),
+            _0xdf213d = new RegExp("[jTrWWKxDNTMSkTJMybwMkSQJWSwPtXUyGzjGt]", "g"),
+            _0xdc0d03 = _0x3b87fb(0x1a2)["replace"](_0xdf213d, "")[_0x3b87fb(0x1a7)](";"),
+            _0x3bf375,
+            _0x521e06,
+            _0x30c74d,
+            _0x326167,
+            _0x45d741 = function (_0x3c3187, _0x528fec, _0x464680) {
+                var _0x1a6425 = _0x3b87fb;
+                if (_0x3c3187[_0x1a6425(0x1a1)] != _0x528fec) return ![];
+                for (var _0x56e540 = 0x0; _0x56e540 < _0x528fec; _0x56e540++) {
+                    for (var _0x48c88a = 0x0; _0x48c88a < _0x464680[_0x1a6425(0x1a1)]; _0x48c88a += 0x2) {
+                        if (_0x56e540 == _0x464680[_0x48c88a] && _0x3c3187[_0x1a6425(0x16d)](_0x56e540) != _0x464680[_0x48c88a + 0x1]) return ![];
+                    }
+                }
+      
+            }
+        
+        
+    });
+_0x6ca684(),
+    $(document)[_0x33563e(0x199)](function () {
+        var _0x47e6fe = _0x33563e;
+        $(document)["on"](_0x47e6fe(0x152), _0x47e6fe(0x186), function () {
+            var _0x592200 = _0x47e6fe;
+            Swal[_0x592200(0x194)](_0x592200(0x171), _0x592200(0x181), "info");
+        }),
+            $(_0x47e6fe(0x18c))["on"](_0x47e6fe(0x17d), function (_0xa4e18) {
+                var _0x2669d9 = _0x47e6fe,
+                    _0x86488d = _0xa4e18[_0x2669d9(0x166)] || _0xa4e18[_0x2669d9(0x193)];
+                if (_0x86488d === 0xd) return _0xa4e18[_0x2669d9(0x18d)](), ![];
+            });
+        var _0x4c163c = $("#search")[_0x47e6fe(0x153)]();
+        $(".grid-section\x20.grid-item-wrapper")[_0x47e6fe(0x18f)](function () {
+            var _0x500bc0 = _0x47e6fe;
+            $(this)[_0x500bc0(0x182)]()[_0x500bc0(0x15a)](new RegExp(_0x4c163c, "i")) < 0x0 ? $(this)[_0x500bc0(0x1a6)]() : $(this)[_0x500bc0(0x161)]();
+        }),
+            $(_0x47e6fe(0x19e))[_0x47e6fe(0x15f)](function () {
+                var _0x4c87af = _0x47e6fe,
+                    _0x2035f3 = $(this)[_0x4c87af(0x153)]();
+                $(_0x4c87af(0x17f))["each"](function () {
+                    var _0x10e386 = _0x4c87af;
+                    $(this)[_0x10e386(0x182)]()[_0x10e386(0x15a)](new RegExp(_0x2035f3, "i")) < 0x0 ? $(this)[_0x10e386(0x1a6)]() : $(this)[_0x10e386(0x161)]();
+                });
+            }),
+            $(_0x47e6fe(0x151))["click"](function () {
+                var _0x1ef77f = _0x47e6fe;
+                $(_0x1ef77f(0x19f))["append"]("<div\x20id=\x22offer-modal-wrapper\x22></div>"),
+                    ($locker_id = $(this)[_0x1ef77f(0x15e)](_0x1ef77f(0x150))),
+                    ($selected_offer_value = $(this)[_0x1ef77f(0x156)](_0x1ef77f(0x19c))[_0x1ef77f(0x182)]()),
+                    ($selected_offer_stats = $(this)["find"](_0x1ef77f(0x158))[_0x1ef77f(0x182)]()),
+                    ($selected_offer_description = $(this)["find"](_0x1ef77f(0x192))[_0x1ef77f(0x182)]()),
+                    ($offer_code_name = $(document)[_0x1ef77f(0x156)]("#offercodename")[_0x1ef77f(0x182)]()),
+                    ($offer_code = $offer_code_name[_0x1ef77f(0x183)](0x0, 0x4)),
+                    _0x1ccccd("rym-s-1", function (_0x149213) {
+                        var _0x15be8f = _0x1ef77f;
+                        $("#offer-modal-wrapper")[_0x15be8f(0x159)](_0x149213)[_0x15be8f(0x165)]()[_0x15be8f(0x17b)](),
+                            $(_0x15be8f(0x18b))[_0x15be8f(0x17a)](_0x15be8f(0x155) + $locker_id + _0x15be8f(0x14e)),
+                            $(_0x15be8f(0x170))["text"]($selected_offer_value),
+                            $("#modal-offer-title")[_0x15be8f(0x182)]($selected_offer_description),
+                            $("#modal-offer-stats")[_0x15be8f(0x182)]($selected_offer_stats),
+                            $(".locked-code-wrapper\x20span")[_0x15be8f(0x182)]($offer_code + "-83MDS"),
+                            $["magnificPopup"][_0x15be8f(0x196)]({
+                                preloader: ![],
+                                type: _0x15be8f(0x169),
+                                modal: !![],
+                                fixedBgPos: !![],
+                                fixedContentPos: !![],
+                                mainClass: _0x15be8f(0x174),
+                                items: { src: _0x15be8f(0x173) },
+                                callbacks: {
+                                    open: function () {
+                                        var _0x207572 = _0x15be8f;
+                                        $(_0x207572(0x195))[_0x207572(0x152)](function () {
+                                            var _0x4ee324 = _0x207572;
+                                            $(_0x4ee324(0x187))[_0x4ee324(0x1a6)](function () {
+                                                var _0xf38a57 = _0x4ee324;
+                                                $(_0xf38a57(0x1a3))[_0xf38a57(0x17b)](), $(".ProgressBar--animateAll")[_0xf38a57(0x1a8)]();
+                                            }),
+                                                ($["fn"][_0x4ee324(0x1a8)] = function (_0x23ca7e) {
+                                                    var _0x5b5a5 = _0x4ee324;
+                                                    _0x23ca7e = $[_0x5b5a5(0x179)]({ animate: !![], animateText: !![] }, _0x23ca7e);
+                                                    var _0x2ba9fe = $(this),
+                                                        _0x9e5a0e = _0x2ba9fe,
+                                                        _0x598115 = _0x9e5a0e[_0x5b5a5(0x156)](".ProgressBar-percentage--count"),
+                                                        _0x13a017 = _0x9e5a0e[_0x5b5a5(0x156)](_0x5b5a5(0x172)),
+                                                        _0x1f5761 = _0x9e5a0e[_0x5b5a5(0x15e)](_0x5b5a5(0x190)),
+                                                        _0xc65aa0 = 0x64 - _0x1f5761,
+                                                        _0x93b1b7 = _0x598115[_0x5b5a5(0x191)]()["attr"]("data-progress"),
+                                                        _0x22aaf7 = _0x13a017[_0x5b5a5(0x15e)]("r"),
+                                                        _0x49c031 = _0x22aaf7 * 0x2,
+                                                        _0x52640f = Math[_0x5b5a5(0x176)](Math["PI"] * _0x49c031),
+                                                        _0x28e337 = (_0x52640f * _0xc65aa0) / 0x64;
+                                                    _0x13a017[_0x5b5a5(0x197)]({ "stroke-dasharray": _0x52640f, "stroke-dashoffset": _0x28e337 }),
+                                                        _0x23ca7e[_0x5b5a5(0x16c)] === !![] && _0x13a017[_0x5b5a5(0x197)]({ "stroke-dashoffset": _0x52640f })["animate"]({ "stroke-dashoffset": _0x28e337 }, 0x1388),
+                                                        _0x23ca7e[_0x5b5a5(0x154)] === !![]
+                                                            ? $({ Counter: 0x0 })[_0x5b5a5(0x16c)](
+                                                                  { Counter: _0x93b1b7 },
+                                                                  {
+                                                                      duration: 0x1388,
+                                                                      step: function () {
+                                                                          var _0x2999a5 = _0x5b5a5;
+                                                                          _0x598115[_0x2999a5(0x182)](Math[_0x2999a5(0x157)](this[_0x2999a5(0x16b)]) + "%");
+                                                                      },
+                                                                      done: function () {
+                                                                          call_locker();
+                                                                      },
+                                                                  }
+                                                              )
+                                                            : _0x598115[_0x5b5a5(0x182)](_0x93b1b7 + "%");
+                                                });
+                                        });
+                                    },
+                                },
+                            });
+                    });
+            });
+        function _0x34e010(_0x4b48e2, _0x31895c, _0x4b32e4) {
+            var _0x44292c = _0x47e6fe,
+                _0x452e00 = $(_0x4b48e2);
+            _0x452e00[_0x44292c(0x15c)]("animated\x20" + _0x31895c),
+                _0x452e00[_0x44292c(0x19d)]("animationend", function () {
+                    var _0x3afe3f = _0x44292c;
+                    $(this)[_0x3afe3f(0x167)]("animated\x20" + _0x31895c), _0x4b32e4 && _0x4b32e4();
+                });
+        }
+        function _0x1ccccd(_0x48fa1a, _0x2bfac0) {
+            var _0xbff0f2 = _0x47e6fe,
+                _0x2cb983 = new XMLHttpRequest();
+            _0x2cb983["open"](_0xbff0f2(0x164), "ryms/" + _0x48fa1a + _0xbff0f2(0x16f)),
+                _0x2cb983["setRequestHeader"](_0xbff0f2(0x189), _0xbff0f2(0x175)),
+                _0x2cb983[_0xbff0f2(0x180)](_0xbff0f2(0x1a0), function () {
+                    var _0x474b04 = _0xbff0f2;
+                    _0x2cb983[_0x474b04(0x163)] == 0x4 && _0x2bfac0 && _0x2bfac0(_0x2cb983[_0x474b04(0x1a5)]);
+                }),
+                _0x2cb983[_0xbff0f2(0x18a)](),
+                console[_0xbff0f2(0x185)](),
+                console["log"](_0xbff0f2(0x160));
+        }
+    });
---
+++ promo/shein.php
@@ -0,0 +1,182 @@
+﻿<!--
+
+Website Designed by CounterMind on Marketing-Rhino.com
+https://www.marketing-rhino.com/
+It is forbidden to re-sell this landing page without Author Permission.
+
+ -->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	
+<!-- Mirrored from ezzycoupon.com/ezycoupon/shein.php.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 06 Aug 2024 15:30:39 GMT -->
+
+<!-- Mirrored from savebig50.com/shein.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 Oct 2024 21:09:11 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+		<title>Shein</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<meta name="description" content="Shein - devilcoupon.com">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="icon" type="image/ico" href="img/favicon.html">
+		<!-- Open Graph Meta Tags-->
+		<meta property="og:title" content="Shein - devilcoupon.com"> <!-- Title which is displayed when your site is shared on social networks -->
+		<meta property="og:description" content="Shein - devilcoupon.com"> <!-- Website description which is displayed when your site is shared on social networks -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://www.devilcoupon.com/"> <!-- Your Website URL -->
+		<meta property="og:image" content="www.ezzycoupon.com/img/logo.html">	<!-- Absolute Path to the Image which will display, when your website is shared on social networks -->
+		<!-- Twitter Meta -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@tweetname">
+		<meta name="twitter:title" content="Tweet Title">
+		<meta name="twitter:description" content="Tweet Content">
+		<meta name="twitter:image" content="ww25.mywebsiteurl.com/twitter-share-imagefb2a.html">
+		<!-- Icons -->
+		<link rel="stylesheet" href="free/1.0.0/icon-font.min.css">
+		<link rel="stylesheet" href="ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- Google Fonts -->
+		<link href="css2920c920c.html?family=Inter:wght@300;400;700;900&amp;display=swap" rel="stylesheet">
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">
+		<link href="css/magnific-popup.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	</head>
+	<body class="single-offer-page">
+		<div id="offercodename">Shein</div>
+		<header>
+			<div class="container">
+				<div class="header-content">
+					<div class="logo-wrapper">
+						<a href='index-2.html'><img src="img/logo.html" class="img-fluid logo-img" alt=""></a>
+					</div>
+					<div class="search-wrapper">
+						<div class="search-form-wrapper">
+							<form id="search-form-subpage" action="https://ezzycoupon.com/ezycoupon/index.php" method="GET">
+								<div class="search-input-wrapper">
+									<i class="fa fa-search"></i>
+									<input type="text" name="search-term" class="search" id="search" value="" placeholder="Search Deals...">
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="header-nav-wrapper">
+						<ul id="header-nav">
+							<li>Categories<i class="fa fa-angle-down"></i></li>
+							<li>Log In</li>
+							<li class="nav-button">Join for Free</li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</header>
+		<section class="single-offer-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3">
+						<div class="sidebar-wrapper">
+							<div class="sidebar-content">
+								<div class="offer-image-wrapper">
+									<img src="img/single-offer/shein1.jpg" class="img-fluid single-offer-img">
+								</div>
+								<div class="sidebar-separator"></div>
+								<div class="offer-rating-wrapper">
+									<div class="rating-wrapper">
+										<span class="text-rating">4.5</span>
+										<span class="rating"><i class="fa fa-rating s-4-5"></i></span>
+										<span class="rating-amount">(385)</span>
+									</div>
+									<span class="rating-note"><strong>Note:</strong> Rating is only available for registered users.</span>
+								</div>
+								<div class="sidebar-separator"></div>
+								<div class="offer-description-wrapper">
+									<h4>About Shein</h4>
+									<div class="offer-description">
+										<p>SHEIN is for you Incomparable quality, best price, free shipping and easy ordering.</p>
+									</div>
+								</div>
+								<div class="sidebar-separator"></div>
+								<div class="offer-saving-tips">
+									<h5>Savings Tips:</h5>
+									<p>Up to 60% Off Select Styles - Join The Shein Mailing List To Receive Insider Info on Sales & New Arrivals</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-9">
+						<div class="offer-wrapper">
+							<div class="offer-content">
+								<div class="offer-title-wrapper">
+									<h1>Shein's Coupons & Deals</h1>
+									<h5>Save with Shein's Offers.</h5>
+								</div>
+								<div class="offers">
+									<div class="offer-row-item" data-l-id="2lmjjp">
+										<div class="offer-row-item-content">
+											<div class="offer-row-value">
+												<span class="value-text-big">100%</span> <span class="value-text-big break-line">OFF</span>
+											</div>
+											<div class="offer-row-content">
+												<div class="offer-row-content-type">Code</div>
+												<div class="offer-row-content-description">Shein 100% Off Entire Order Coupon Code</div>
+												<div class="offer-row-content-stats">Verified • 34K users are saying this works 👍</div>
+											</div>
+											<div class="offer-row-button-wrapper">
+												<button type="reset" onclick="location.href='https://appinstallcheck.com/cl/i/lk13od'">
+													<div class="offer-row-button">
+														Show Coupon Code
+														<div class="button-decor-1"></div>
+														<div class="button-decor-2"></div>
+													</div>
+												</button>
+											</div>
+											
+											<div class="clearfix"></div>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<div class="footer-content">
+				<div class="container">
+					<img src="img/logo.html" class="img-fluid footer-logo-img" alt="">
+					<ul class="footer-nav">
+						<li><a href="#">Terms of Use</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Sitemap</a></li>
+					</ul>
+					<ul class="footer-nav footer-social-nav">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+					</ul>
+					<span class="copyright">©2020 devilcoupon.com</span>
+				</div>
+			</div>
+			<div class="trademark-content">
+				<div class="container">
+					Third-party trademarks are the property of their respective third-party owners.<br>
+					Presence of a third-party trademark does not mean that IcyCoupons has any relationship with that third-party or that the third-party endorses IcyCoupons or its services.
+				</div>
+			</div>
+		</footer>
+	</body>
+	<!-- JS -->
+	<script type="text/javascript" src="ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="npm/sweetalert2%409.html"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.html"></script>
+    <script type="text/javascript" src="js/validator.min.html"></script>
+	<script type="text/javascript" src="js/main-fix.html"></script>
+
+<!-- Mirrored from ezzycoupon.com/ezycoupon/aliexpress.php.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 06 Aug 2024 15:30:40 GMT -->
+
+<!-- Mirrored from savebig50.com/shein.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 Oct 2024 21:09:12 GMT -->
+</html>
